c170a177aa6855fcfeda6054d8bd4a1ef48376a6
Zahid <zahimdhussain786@gmail.com>
2022-11-17 12:34:02 +0530

    Add files to Github

---
+++ Microsoft_clone/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1920px',
+        '2xxl': '1700px',
+      },
+      spacing: {
+      },
+    },
+  },
+  plugins: [],
+}
